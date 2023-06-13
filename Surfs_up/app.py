8ebb48e041e47ea431d41175f82699067aafc464
RPF35 <126990642+RPF35@users.noreply.github.com>
2023-06-12 21:42:15 -0400

    update app.py

--- Surfs_up/app.py
+++ Surfs_up/app.py
@@ -1,29 +1,66 @@
 # Import the dependencies.
-from flask import Flask
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
 
 #################################################
 # Database Setup
 #################################################
 
+# Set up the database connection
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect the database tables
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
-# reflect the tables
+# Save references to the measurement and station tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
 
 #################################################
 # Flask Setup
 #################################################
-
-
+# Instance of Flask app
+app= Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+# Define route to retrieve measurements 
+@app.route('/api/measurements')
+def get_measurements():
+    # Query the measurement table
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    # Convert the query results to a list of dictionaries
+    measurements = []
+    for date, prcp in results:
+        measurement = {'date': date, 'prcp': prcp}
+        measurements.append(measurement)
+
+    # Return the measurements as JSON
+    return jsonify(measurements)
+
+# Define a route to retrieve stations
+@app.route('/api/stations')
+def get_stations():
+    # Query the station table
+    results = session.query(Station.station, Station.name).all()
+
+    # Convert the query results to a list of dictionaries
+    stations = []
+    for station, name in results:
+        station_data = {'station': station, 'name': name}
+        stations.append(station_data)
+
+# Run Flask app
+if __name__ == '__main__':
+    app.run()
